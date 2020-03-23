21cb0c25d9a82ca65c933477e4f1b3914c6e495f
Daniel Mendes <dadinel@hotmail.com>
2020-03-23 12:03:48 -0300

    Create break_string_to_array.js

---
+++ js/441112/break_string_to_array.js
@@ -0,0 +1,24 @@
+function breakString(string, size) {
+  //Valor default de 5000
+  size = size || 5000;
+
+  breakStr = [];
+
+  while (string.length > 0) {
+    breakStr.push(string.substring(0,size));
+    string = string.substring(size,string.length);
+  }
+
+  return breakStr;
+}
+
+let bigString = "abcdefghij".repeat(2000);
+
+//Quebrar com o tamanho padrão de  5000
+console.log(breakString(bigString));
+
+//Quebrar de 130 em 130...
+console.log(breakString(bigString, 130));
+
+//Exemplo para verificar se a string de entrada é a mesma de saída
+console.log( bigString == breakString(bigString, 147).reduce( (acc, value) => acc += value , "") );
